11f603d321979e63da09536d4b1dd99d08e5183d
Rebecca Vessal <rebeccaVessal@gmail.com>
2013-11-19 17:52:43 -0500

    Fixed shaders after merge conflict

--- CatBowling/_Binary/Shaders/vshader.glsl
+++ CatBowling/_Binary/Shaders/vshader.glsl
@@ -18,11 +18,7 @@ uniform mat4 projection;
 void main() 
 {
 	vec4 offset = vec4(newX, newY, newZ, 0.0);
-<<<<<<< HEAD
     gl_Position = projection * model_view * transformationMatrix * ((vPosition + offset));
 	gl_Position /= vPosition.w;
-=======
-    gl_Position =  projection * model_view * (vPosition + offset)/vPosition.w;
->>>>>>> 479d05018c1419c1500cdaaef51e5ca4402f25fa
 	fColor = vColor;
 } 
